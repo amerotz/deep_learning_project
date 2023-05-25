1414343a3b5b9d2611b71aa20ba261a8e70424e6
Linus Ostlund <linusostlund2@gmail.com>
2023-05-25 09:53:39 +0200

    Move all write-to-file logic to create_dataset.py and only load existing data in dataset.py

--- src/data_processing/create_dataset.py
+++ src/data_processing/create_dataset.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import os
 from collections import defaultdict
+import json
 
 ignore = [
     "^F,,",
@@ -67,3 +68,104 @@ for path_to_file in tqdm(os.listdir("abc")):
 
 with open("data/dataset.txt", "w") as f:
     f.write(data)
+
+
+########### Moved this from create_dataset.py ############
+
+
+def transform_data_to_json(data_file: str, max_sequence_length: int = 256) -> None:
+    """
+    Create dataset from `data_file`. In particular:
+    - create w2i and i2w
+    - create actual dataset input and target pairs
+    """
+    data = []
+    w2i = defaultdict(int)
+    i2w = defaultdict(str)
+
+    print("Creating dataset...")
+    with open("data/dataset.txt", "r") as f:
+        # read the file and split in lines
+        # 1 line = 1 piece
+        lines = f.read().split("\n")
+
+        # filter empty strings
+        lines = list(filter(None, lines))
+
+        # index pad, sos and eos tokens
+        tok_id = 0
+        w2i["<pad>"] = tok_id
+        i2w[tok_id] = "<pad>"
+
+        tok_id += 1
+        w2i["<sos>"] = tok_id
+        i2w[tok_id] = "<sos>"
+
+        tok_id += 1
+        w2i["<eos>"] = tok_id
+        i2w[tok_id] = "<eos>"
+
+        tok_id += 1
+
+        print("Creating w2i and i2w...")
+        # create w2i and i2w
+        for piece in tqdm(lines):
+            tokens = piece.split(" ")
+            tokens = list(filter(None, tokens))
+
+            for tok in tokens:
+                # add token to dicts
+                if not tok in w2i:
+                    w2i[tok] = tok_id
+                    i2w[tok_id] = tok
+                    tok_id += 1
+
+        # add the pieces to data using w2i
+        # add <sos> at the start
+        # add <eos> at the end
+        # pad with <pad> until max length
+        print("Indexing pieces...")
+        for piece in tqdm(lines):
+            tokens = piece.split(" ")
+            tokens = list(filter(None, tokens))
+
+            # input
+            input = ["<sos>"] + tokens
+            input = input[: max_sequence_length]
+
+            # target
+            target = tokens[: max_sequence_length - 1]
+            target = target + ["<eos>"]
+
+            # pad everything
+            length = len(input)
+            input.extend(["<pad>"] * (max_sequence_length - length))
+            target.extend(["<pad>"] * (max_sequence_length - length))
+
+            # convert to indexes
+            input = [w2i.get(w) for w in input]
+            target = [w2i.get(w) for w in target]
+
+            # add dict to data
+            item = {
+                "input": input,
+                "target": target,
+            }
+            data.append(item)
+
+    # save data to json
+    print("Writing data to json...")
+    with open("data/dataset.json", "w") as f:
+        data = json.dumps(data)
+        f.write(data)
+
+    # save vocabs to json
+    print("Writing vocabs to json...")
+    with open("data/vocab.json", "w") as f:
+        vocab = {"w2i": w2i, "i2w": i2w}
+        data = json.dumps(vocab)
+        f.write(data)
+
+    print("Done")
+
+transform_data_to_json("data/dataset.txt")
--- src/dataset.py
+++ src/dataset.py
@@ -10,8 +10,7 @@ class MusicDataset(torch.utils.data.Dataset):
         self,
         data_file: str,
         max_sequence_length: int,
-        vocab_file=None,
-        create_data=True,
+        vocab_file: str,
     ):
         """
         Create the dataset. If `create_data==True`, `data_file`
@@ -21,105 +20,8 @@ class MusicDataset(torch.utils.data.Dataset):
         super().__init__()
 
         self.max_sequence_length = max_sequence_length
-        if create_data:
-            self.w2i = defaultdict(int)
-            self.i2w = defaultdict(str)
-            self.data = []
-            self._create_data(data_file)
-        else:
-            self._load_data(data_file, vocab_file)
 
-    def _create_data(self, data_file: str):
-        """
-        Create dataset from `data_file`. In particular:
-        - create w2i and i2w
-        - create actual dataset input and target pairs
-        """
-
-        print("Creating dataset...")
-        with open(data_file, "r") as f:
-            # read the file and split in lines
-            # 1 line = 1 piece
-            lines = f.read().split("\n")
-
-            # filter empty strings
-            lines = list(filter(None, lines))
-
-            # index pad, sos and eos tokens
-            tok_id = 0
-            self.w2i["<pad>"] = tok_id
-            self.i2w[tok_id] = "<pad>"
-
-            tok_id += 1
-            self.w2i["<sos>"] = tok_id
-            self.i2w[tok_id] = "<sos>"
-
-            tok_id += 1
-            self.w2i["<eos>"] = tok_id
-            self.i2w[tok_id] = "<eos>"
-
-            tok_id += 1
-
-            print("Creating w2i and i2w...")
-            # create w2i and i2w
-            for piece in tqdm(lines):
-                tokens = piece.split(" ")
-                tokens = list(filter(None, tokens))
-
-                for tok in tokens:
-                    # add token to dicts
-                    if not tok in self.w2i:
-                        self.w2i[tok] = tok_id
-                        self.i2w[tok_id] = tok
-                        tok_id += 1
-
-            # add the pieces to data using w2i
-            # add <sos> at the start
-            # add <eos> at the end
-            # pad with <pad> until max length
-            print("Indexing pieces...")
-            for piece in tqdm(lines):
-                tokens = piece.split(" ")
-                tokens = list(filter(None, tokens))
-
-                # input
-                input = ["<sos>"] + tokens
-                input = input[: self.max_sequence_length]
-
-                # target
-                target = tokens[: self.max_sequence_length - 1]
-                target = target + ["<eos>"]
-
-                # pad everything
-                length = len(input)
-                input.extend(["<pad>"] * (self.max_sequence_length - length))
-                target.extend(["<pad>"] * (self.max_sequence_length - length))
-
-                # convert to indexes
-                input = [self.w2i.get(w) for w in input]
-                target = [self.w2i.get(w) for w in target]
-
-                # add dict to data
-                item = {
-                    "input": input,
-                    "target": target,
-                }
-                self.data.append(item)
-
-        # save data to json
-        print("Writing data to json...")
-        with open("data/dataset.json", "w") as f:
-            data = json.dumps(self.data)
-            f.write(data)
-
-        # save vocabs to json
-        print("Writing vocabs to json...")
-        with open("data/vocab.json", "w") as f:
-            vocab = {"w2i": self.w2i, "i2w": self.i2w}
-            data = json.dumps(vocab)
-            f.write(data)
-
-        print("Done")
+        self._load_data(data_file, vocab_file)
 
     @property
     def sos_idx(self):
